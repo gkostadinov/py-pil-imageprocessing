267658d271618e7a4504534798d1835643429307
Georgi Kostadinov <grgkostadinov@gmail.com>
2016-01-18 18:51:09 +0200

    Add another method for iterating image pixels - using Image.getdata

--- 0-basics/2.iterate_pixels.py
+++ 0-basics/2.iterate_pixels.py
@@ -8,7 +8,7 @@ def main():
 
     width, height = image.size
 
-    # Iterate the pixels
+    # Method 1: Iterate the pixels
     for x in range(width):
         for y in range(height):
             # Get pixel at coords (x, y)
@@ -17,6 +17,15 @@ def main():
 
             r, g, b = pixel
             print('image[%s, %s] = rgb(%s, %s, %s)' % (x, y, r, g, b))
+
+    # Method 2: Using Image.getdata
+    pixel_data = image.getdata()
+    for i, pixel in enumerate(pixel_data):
+        x = i % width
+        y = i / width
+
+        r, g, b = pixel
+        print('image[%s, %s] = rgb(%s, %s, %s)' % (x, y, r, g, b))
 
 
 if __name__ == '__main__':
